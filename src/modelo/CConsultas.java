efe274e5b2b0194bbe72974e6a9d119b6fdf10b6
juliocmezar <jmeza@umanizales.edu.co>
2025-10-17 08:41:53 -0500

    versión funcional oct 17

--- src/modelo/CConsultas.java
+++ src/modelo/CConsultas.java
@@ -40,28 +40,7 @@ public class CConsultas {
             //ejecuto la consulta luego de prepararla
             ResultSet resultado = preparar.executeQuery();
 
-            /* //Este código me sirve para ver la salida en consola, para probar si la consulta está bien hecha
-          //la metadata sirve para ver el nombre de las colomnas, el tipo de dato y cuantas colomnas tiene la consulta
-          ResultSetMetaData metaData = resultado.getMetaData();
-          //traigo el número de columnas
-          int numeroColumnas = metaData.getColumnCount();
-          for (int i = 1; i <= numeroColumnas; i++) {
-                //imprimo el nombre de las columnas
-                System.out.print(metaData.getColumnName(i) + "\t");
-          }
-          System.out.println();
-          //imprimo las filas
-          while(resultado.next()){           
-            for (int i = 1; i <= numeroColumnas; i++) {
-                    System.out.print(resultado.getString(i) + "\t");
-                }
-                System.out.println();
-          }
-          System.out.println("Consulta correcta");
-
-             */ //fin del código para probar consulta
-            //recorro cada fila, se intancia un objeto de la clase CContacto y se guarda en la lista
-            while (resultado.next()) {
+                      while (resultado.next()) {
                 CContacto c = new CContacto(
                         resultado.getInt("id"),
                         resultado.getString("nombres"),
